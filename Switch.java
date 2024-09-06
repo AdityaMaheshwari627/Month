99e50430953b2374846a62feada7c2f5a579cbdd
AdityaMaheshwari627 <aditya27mh@gmail.com>
2024-09-06 23:30:12 +0530

    Add files via upload
    Find a Month by numbering

---
+++ Switch.java
@@ -0,0 +1,42 @@
+//Get product using Switch Statement
+package Numbers;
+
+import java.util.Scanner;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner obj=new Scanner(System.in);
+        System.out.print("Enter num:");
+        int num=obj.nextInt();
+        switch(num) {
+        case 1:System.out.print("January");
+        break;
+        case 2:System.out.print("February");
+        break;
+        case 3:System.out.print("March");
+        break;
+        case 4:System.out.print("April");
+        break;
+        case 5:System.out.print("May");
+        break;
+        case 6:System.out.print("June");
+        break;
+        case 7:System.out.print("July");
+        break;
+        case 8:System.out.print("August");
+        break;
+        case 9:System.out.print("September");
+        break;
+        case 10:System.out.print("October");
+        break;
+        case 11:System.out.print("November");
+        break;
+        case 12:System.out.print("December");
+        break;
+        default:System.out.print("Not exist");
+        }
+	}
+
+}
